2f97637b21a179de87e4de8de8e382c24640f9c5
PaoloAttardi <attapao@gmail.com>
2023-04-30 15:53:00 +0200

    trained yolo

--- objectDetection.py
+++ objectDetection.py
@@ -1,15 +1,18 @@
 import torch
-from IPython.display import Image  # for displaying images
-import os 
-import random
-import shutil
-from sklearn.model_selection import train_test_split
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
-from tqdm import tqdm
-from PIL import Image, ImageDraw
-import numpy as np
-import matplotlib.pyplot as plt
 
-random.seed(108)
+# Model
+model = torch.hub.load('yolov5', 'yolov5s', source='local')
+
+# Image
+im = r"C:\Users\Computer\Desktop\prova.jpg"
+
+# Inference
+results = model(im)
+
+results.pandas().xyxy[0]
+#      xmin    ymin    xmax   ymax  confidence  class    name
+# 0  749.50   43.50  1148.0  704.5    0.874023      0  person
+# 1  433.50  433.50   517.5  714.5    0.687988     27     tie
+# 2  114.75  195.75  1095.0  708.0    0.624512      0  person
+# 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
 
