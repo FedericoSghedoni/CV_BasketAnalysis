b20592a542f94c13c6d6ae2fe39780d381197748
FedericoSghedoni <sghe99@gmail.com>
2023-05-30 16:03:55 +0200

    Train Dataset improvement

--- dataset/check.py
+++ dataset/check.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python
-# import required module
 import os
 # assign directory
-directory = 'images'
-sourcedir = 'labels'
-destdir = 'checked'
+directory = 'dataset/train/images'
+sourcedir = 'dataset/train/labels'
+destdir = 'dataset/train/checked'
 # iterate over files in
 # that directory
 for filename in os.listdir(directory):
     size = len(filename)
     name = filename[:size - 3]
     target = name + 'txt'
+    print(target)
     for files in os.listdir(sourcedir):
-        if target not in files:
-            src = os.path.join(sourcedir, target)
-            dest = os.path.join(destdir, target)
+        if target in files:
+            src = sourcedir + '/' + target
+            dest = destdir + '/' + target
             os.rename(src, dest)
