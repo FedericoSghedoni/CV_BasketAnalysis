5ed1f32b47a6ac5298063ed04af2a87588bbfe8a
PaoloAttardi <81446798+PaoloAttardi@users.noreply.github.com>
2023-12-01 19:22:10 +0100

    more videos

--- modelTrainer.py
+++ modelTrainer.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.optim as optim
 import torch
@@ -15,8 +18,6 @@ def report(csv_file,data):
     with open(csv_file, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         existing_data = list(csvreader)
-    if existing_data == []:
-        existing_data = [['epoch', 'loss', 'y_pred', 'y_true']]
     existing_data.append(data)
     with open(csv_file, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
@@ -33,7 +34,8 @@ def YoloTrainer():
 
 def TrasformerTrainer():
 
-    csv_file = 'output.csv'
+    csv_train_file = 'result/train_output.csv'
+    csv_test_file = 'result/test_output.csv'
     transformer = Transformer(tgt_size=1, n_feature=9,  d_model=160)
 
     # We use the Binary Cross Entropy since we have a 2 class problem
@@ -48,8 +50,15 @@ def TrasformerTrainer():
     test_dataloader = DataLoader(test, batch_size=32)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    for epoch in range(100):
-        print(f'Current Number of epoch: {epoch}')
+    with open(csv_train_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows([['epoch','loss']])
+
+    with open(csv_test_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows([['epoch','loss']])
+
+    for epoch in range(101):
         epoch_losses = []
         transformer.train()
         # Select one video at time
@@ -57,14 +66,9 @@ def TrasformerTrainer():
             for idx in range(datapoint['label'].shape[0]):
                 # Add a dimension for the batch dimension, in this implemantion is 1
                 inputs = datapoint['emb_fea'][idx].unsqueeze(0)
-
-                # Add padding to have always the same input dimension
-                # 112 stand for the src_dimension/9 the feature number
-                # padd = 112 - inputs.shape[1]
-                # inputs = torch.nn.functional.pad(inputs, (0,0,0,padd), mode='constant', value=0)
-
                 labels = datapoint['label'][idx]
                 inputs, labels = inputs.to(device), labels.to(device)
+
                 optimizer.zero_grad()
                 outputs = transformer(inputs)
                 # print(f'Output from the model and true label: {outputs, labels}')
@@ -75,7 +79,7 @@ def TrasformerTrainer():
         print(f">>> Epoch {epoch} train loss: ", np.mean(epoch_losses))
 
         data = [epoch,np.mean(epoch_losses)]
-        report(csv_file,data)
+        report(csv_train_file,data)
 
         if epoch % 5 == 0:
             epoch_losses = []
@@ -85,18 +89,24 @@ def TrasformerTrainer():
                 for idx in range(test_datapoint['label'].shape[0]):
                     # Select one video at time, repeat the same steps as before 
                     inputs = test_datapoint['emb_fea'][idx].unsqueeze(0)
-
-                    # Add padding to have always the same input dimension
-                    # 112 stand for the src_dimension/9 the feature number
-                    # padd = 112 - inputs.shape[1]
-                    # inputs = torch.nn.functional.pad(inputs, (0,0,0,padd), mode='constant', value=0)
-
                     labels = test_datapoint['label'][idx]   
                     inputs, labels = inputs.to(device), labels.to(device)
+
                     outputs = transformer(inputs)
                     loss = criterion(outputs, labels)
                     epoch_losses.append(loss.item())
             print(f">>> Epoch {epoch} test loss: ", np.mean(epoch_losses))
+
+            data = [epoch,np.mean(epoch_losses)]
+            report(csv_test_file,data)
+    
+    df = pd.read_csv('train_output.csv')
+    sns.lineplot(data=df,x='epoch',y='loss')
+    plt.savefig('result/train_result.png')
+
+    df = pd.read_csv('test_output.csv')
+    sns.lineplot(data=df,x='epoch',y='loss')
+    plt.savefig('result/test_result.png')
 
     return transformer
 
